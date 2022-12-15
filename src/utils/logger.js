9c349be3a3730cc4701cdad48f139a41eb83715d
Divya Shet <88928587+Divyashet66@users.noreply.github.com>
2022-12-16 01:53:01 +0530

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,12 +1,29 @@
-const { createLogger, format, transports } = require('winston');
+// const { createLogger, format, transports } = require('winston');
+const winston = require('winston');
 
-module.exports = createLogger({
-transports:
-    new transports.File({
-    filename: 'logs/server.log',
-    format:format.combine(
-        format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
-        format.align(),
-        format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
-    )}),
-});
+// module.exports = createLogger({
+// transports:
+//     new transports.File({
+//     filename: 'logs/server.log',
+//     format:format.combine(
+//         format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+//         format.align(),
+//         format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+//     )}),
+//   level: 'info',
+//   format:format.combine(
+//     format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+//     format.align(),
+//     format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+// ),
+
+
+// });
+
+module.exports.logger = winston.createLogger({
+    transports: [
+      new winston.transports.Console()
+    ]
+  });
+  
+
